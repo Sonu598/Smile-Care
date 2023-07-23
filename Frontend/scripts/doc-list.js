ec55909d309476e331e7d6b631bfe864e4cdd5ca
Amitdevda <akd9t7@gmail.com>
2023-07-23 16:06:26 +0530

    Doc list localstorage

--- Frontend/scripts/doc-list.js
+++ Frontend/scripts/doc-list.js
@@ -20,10 +20,10 @@ function doc_card(element){
   <div id="card">
   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-r8Q7-RgTO0jrN80AHUj139ftv3ZeKnpg5Q&usqp=CAU" alt="Doctor Image" />
   <div id="detail">
-          <h1>${element.name} <span style="font-size:x-small;">( MBBS )</span></h1>
+          <h1 id="naam">${element.name} <span style="font-size:x-small;">( MBBS )</span></h1>
           <p>An experience of ${element.experience} years from ${element.location}</p>
           <h4>Fees : <span>${element.fees} /-</span></h4>
-          <a href="./appointment.html"><button>Book Appointment</button></a>
+          <a href="./appointment.html"><button id="btn">Book Appointment</button></a>
       
   </div>
 </div>
@@ -31,4 +31,11 @@ function doc_card(element){
       `;
 }
 
+
+let btn=document.getElementById("btn")
+let naam=document.getElementById("naam").innerText
+
+btn.addEventListener("click",()=>{
+ localStorage.setItem("Doc_name",naam)
+})
 
